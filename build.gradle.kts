82cba757293bf7fdea3c24965b95c27751697f3c
Josinaldo Gomes <josinaldo_jr@hotmail.com>
2025-07-15 16:08:13 +1200

    test: Adiciona classes de teste BaseTest, BreedsTest e RandomImageTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    testImplementation("io.rest-assured:rest-assured:5.3.2")
+    testImplementation("io.rest-assured:json-schema-validator:5.3.2")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
---
+++ src/test/java/br/com/agibank/dogapitestes/tests/BreedsTest.java
@@ -0,0 +1,63 @@
+package br.com.agibank.dogapitestes.tests;
+
+import br.com.agibank.dogapitestes.base.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.*;
+import javax.net.ssl.HttpsURLConnection;
+import static io.restassured.RestAssured.given;
+
+public class BreedsTest extends BaseTest {
+
+    @Test
+    @Tag("positivo")
+    @DisplayName("Deve Listar todas as raças com sucesso")
+    public void deveListarTodasAsRacas(){
+        given()
+
+        .when()
+                .get("/breeds/list/all")
+        .then()
+                .statusCode(HttpsURLConnection.HTTP_OK)
+                .body("message",not(empty()))
+                .body("status",equalTo("success"))
+        ;
+    }
+
+    @Test
+    @Tag("positivo")
+    @DisplayName("Deve listar imagens de raça existente")
+    public void deveListarImagensDeRacaExistente(){
+
+        given()
+                .pathParam("breed","hound")
+        .when()
+                .get("/breed/{breed}/images")
+        .then()
+                .statusCode(HttpsURLConnection.HTTP_OK)
+                .body("status",equalTo("success"))
+                .body("message",not(empty()))
+        ;
+    }
+
+    @Test
+    @Tag("negativo")
+    @DisplayName("Não deve listar imagens com raça inexistente")
+    public void NaoDeveListarImagensDeRacaInexistente(){
+
+        given()
+                .pathParam("breed","raca-inexistente")
+        .when()
+                .get("/breed/{breed}/images")
+        .then()
+                .statusCode(HttpsURLConnection.HTTP_NOT_FOUND)
+                .body("status",equalTo("error"))
+                .body("message",equalTo("Breed not found (master breed does not exist)"))
+        ;
+    }
+
+
+
+
+}
