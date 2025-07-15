57b3262a0dc49184e99082e106cf6e8667636711
Josinaldo Gomes <josinaldo_jr@hotmail.com>
2025-07-15 23:36:31 +1200

    ci: Implementa publicação automática do relatório de testes com Allure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("io.qameta.allure") version "2.11.2"
 }
 
 group = "josinaldo_junior"
@@ -9,12 +10,19 @@ repositories {
     mavenCentral()
 }
 
+allure {
+    version.set("2.27.0")
+}
+
 dependencies {
     testImplementation("io.rest-assured:rest-assured:5.3.2")
     testImplementation("io.rest-assured:json-schema-validator:5.3.2")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testImplementation("io.qameta.allure:allure-junit5:2.27.0")
+    testImplementation("io.qameta.allure:allure-rest-assured:2.27.0")
 }
 
 tasks.test {
